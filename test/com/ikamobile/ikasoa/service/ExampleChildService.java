1ce359f73f88c428234ed40ca4e128e73448be34
Larry <larry7696@gmail.com>
2016-05-13 11:49:17 +0800

    删除无用目录

--- test/com/ikamobile/ikasoa/service/ExampleChildService.java
+++
@@ -1,7 +0,0 @@
-package com.ikamobile.ikasoa.service;
-
-public interface ExampleChildService extends ExampleSuperService {
-
-	public Boolean helloxx();
-
-}
--- test/com/ikamobile/ikasoa/service/ExampleSuperService.java
+++
@@ -1,7 +0,0 @@
-package com.ikamobile.ikasoa.service;
-
-public interface ExampleSuperService {
-
-	public Boolean helloxxx();
-
-}
