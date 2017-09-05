3f60a0bc2df351f41c37b9a923e519b03d55eb96
Larry <larry7696@gmail.com>
2017-09-05 13:52:59 +0800

    optimize code

--- ikasoa-core/src/com/ikasoa/core/thrift/server/ThriftServlet.java
+++ ikasoa-core/src/com/ikasoa/core/thrift/server/ThriftServlet.java
@@ -1,5 +1,12 @@
 package com.ikasoa.core.thrift.server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.thrift.server.TServlet;
 
 /**
@@ -10,8 +17,11 @@ import org.apache.thrift.server.TServlet;
  * @author <a href="mailto:larry7696@gmail.com">Larry</a>
  * @version 0.2
  */
-@SuppressWarnings("serial")
 public class ThriftServlet extends TServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private String serverName;
 
 	/**
 	 * 构造方法
@@ -21,6 +31,21 @@ public class ThriftServlet extends TServlet {
 	 */
 	public ThriftServlet(ThriftServer server) {
 		super(server.getProcessor(), server.getThriftServerConfiguration().getProtocolFactory());
+		this.serverName = server.getServerName();
+	}
+
+	/**
+	 * 
+	 * @author <a href="mailto:larry7696@gmail.com">Larry</a>
+	 * @version 0.5.1
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// super.doPost(request, response);
+		PrintWriter w = response.getWriter();
+		w.write("This is a ikasoa server! (" + this.serverName + ")");
+		w.close();
 	}
 
 }
