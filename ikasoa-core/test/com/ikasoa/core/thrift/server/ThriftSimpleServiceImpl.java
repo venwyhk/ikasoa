70463e543671faff08f935464914a3e67de74d07
Larry <larry7696@gmail.com>
2017-09-05 15:08:06 +0800

    update code comments

--- ikasoa-core/test/com/ikasoa/core/thrift/server/ThriftSimpleServiceImpl.java
+++ ikasoa-core/test/com/ikasoa/core/thrift/server/ThriftSimpleServiceImpl.java
@@ -2,6 +2,9 @@ package com.ikasoa.core.thrift.server;
 
 import org.apache.thrift.TException;
 
+/**
+ * 测试Thrift服务实现
+ */
 public class ThriftSimpleServiceImpl implements ThriftSimpleService.Iface {
 	@Override
 	public String get(String arg) throws TException {
