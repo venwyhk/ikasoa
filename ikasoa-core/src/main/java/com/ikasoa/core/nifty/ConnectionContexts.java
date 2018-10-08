c775dac2fff0dde9b25fe1f54557a31b48b6ffe8
Larry Su <larry7696@gmail.com>
2018-10-08 15:25:43 +1300

    Update ConnectionContexts.java

--- ikasoa-core/src/main/java/com/ikasoa/core/nifty/ConnectionContexts.java
+++ ikasoa-core/src/main/java/com/ikasoa/core/nifty/ConnectionContexts.java
@@ -6,7 +6,8 @@ public class ConnectionContexts {
 	public static ConnectionContext getContext(Channel channel) {
 		ConnectionContext context = (ConnectionContext) channel.getPipeline().getContext(ConnectionContextHandler.class)
 				.getAttachment();
-		Preconditions.checkState(context != null, "Context not yet set on channel {}", channel);
+		if(context == null)
+			throw new IllegalStateException("Context not yet set on channel " + channel.toString());
 		return context;
 	}
 }
