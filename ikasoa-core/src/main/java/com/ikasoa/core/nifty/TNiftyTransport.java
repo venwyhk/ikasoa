b1a6dd68aa68f0bab9430635ea3dfd8759261f0d
Larry Su <larry7696@gmail.com>
2018-10-08 15:09:38 +1300

    Update TNiftyTransport.java

--- ikasoa-core/src/main/java/com/ikasoa/core/nifty/TNiftyTransport.java
+++ ikasoa-core/src/main/java/com/ikasoa/core/nifty/TNiftyTransport.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2012-2016 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.ikasoa.core.nifty;
 
 import org.apache.thrift.TApplicationException;
@@ -22,43 +7,64 @@ import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Wraps incoming channel buffer into TTransport and provides a output buffer.
  */
 public class TNiftyTransport extends TTransport {
-	private final Channel channel;
-	private final ChannelBuffer in;
-	private final ThriftTransportType thriftTransportType;
-	private ChannelBuffer out;
+
 	private static final int DEFAULT_OUTPUT_BUFFER_SIZE = 1024;
+
+	private final Channel channel;
+
+	private final ChannelBuffer inputBuffer;
+
+	@Getter
+	private final ThriftTransportType thriftTransportType;
+
+	@Getter
+	@Setter
+	private ChannelBuffer outputBuffer;
+
 	private final int initialReaderIndex;
+
 	private final int initialBufferPosition;
+
+	@Getter
 	private int bufferPosition;
+
 	private int bufferEnd;
+
+	@Getter
 	private final byte[] buffer;
+
+	@Getter
+	@Setter
 	private TApplicationException tApplicationException;
 
-	public TNiftyTransport(Channel channel, ChannelBuffer in, ThriftTransportType thriftTransportType) {
+	public TNiftyTransport(Channel channel, ChannelBuffer inputBuffer, ThriftTransportType thriftTransportType) {
 		this.channel = channel;
-		this.in = in;
+		this.inputBuffer = inputBuffer;
 		this.thriftTransportType = thriftTransportType;
-		this.out = ChannelBuffers.dynamicBuffer(DEFAULT_OUTPUT_BUFFER_SIZE);
-		this.initialReaderIndex = in.readerIndex();
+		this.outputBuffer = ChannelBuffers.dynamicBuffer(DEFAULT_OUTPUT_BUFFER_SIZE);
+		this.initialReaderIndex = inputBuffer.readerIndex();
 
-		if (!in.hasArray()) {
+		if (!inputBuffer.hasArray()) {
 			buffer = null;
 			bufferPosition = 0;
 			initialBufferPosition = bufferEnd = -1;
 		} else {
-			buffer = in.array();
-			initialBufferPosition = bufferPosition = in.arrayOffset() + in.readerIndex();
-			bufferEnd = bufferPosition + in.readableBytes();
+			buffer = inputBuffer.array();
+			initialBufferPosition = bufferPosition = inputBuffer.arrayOffset() + inputBuffer.readerIndex();
+			bufferEnd = bufferPosition + inputBuffer.readableBytes();
 			// Without this, reading from a !in.hasArray() buffer will advance the
 			// readerIndex
 			// of the buffer, while reading from a in.hasArray() buffer will not advance the
 			// readerIndex, and this has led to subtle bugs. This should help to identify
 			// those problems by making things more consistent.
-			in.readerIndex(in.readerIndex() + in.readableBytes());
+			inputBuffer.readerIndex(inputBuffer.readerIndex() + inputBuffer.readableBytes());
 		}
 	}
 
@@ -90,8 +96,8 @@ public class TNiftyTransport extends TTransport {
 			consumeBuffer(_read);
 			return _read;
 		} else {
-			int _read = Math.min(in.readableBytes(), length);
-			in.readBytes(bytes, offset, _read);
+			int _read = Math.min(inputBuffer.readableBytes(), length);
+			inputBuffer.readBytes(bytes, offset, _read);
 			return _read;
 		}
 	}
@@ -106,19 +112,7 @@ public class TNiftyTransport extends TTransport {
 
 	@Override
 	public void write(byte[] bytes, int offset, int length) throws TTransportException {
-		out.writeBytes(bytes, offset, length);
-	}
-
-	public ChannelBuffer getOutputBuffer() {
-		return out;
-	}
-
-	public void setOutputBuffer(ChannelBuffer buf) {
-		out = buf;
-	}
-
-	public ThriftTransportType getTransportType() {
-		return thriftTransportType;
+		outputBuffer.writeBytes(bytes, offset, length);
 	}
 
 	@Override
@@ -134,37 +128,16 @@ public class TNiftyTransport extends TTransport {
 	}
 
 	@Override
-	public byte[] getBuffer() {
-		return buffer;
-	}
-
-	@Override
-	public int getBufferPosition() {
-		return bufferPosition;
-	}
-
-	@Override
 	public int getBytesRemainingInBuffer() {
 		return bufferEnd - bufferPosition;
 	}
 
 	public int getReadByteCount() {
-		if (getBytesRemainingInBuffer() >= 0) {
-			return getBufferPosition() - initialBufferPosition;
-		} else {
-			return in.readerIndex() - initialReaderIndex;
-		}
+		return getBytesRemainingInBuffer() >= 0 ? getBufferPosition() - initialBufferPosition
+				: inputBuffer.readerIndex() - initialReaderIndex;
 	}
 
 	public int getWrittenByteCount() {
 		return getOutputBuffer().writerIndex();
-	}
-
-	public void setTApplicationException(TApplicationException e) {
-		tApplicationException = e;
-	}
-
-	public TApplicationException getTApplicationException() {
-		return tApplicationException;
 	}
 }
