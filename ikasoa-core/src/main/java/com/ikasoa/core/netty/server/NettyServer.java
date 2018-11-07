85fec0e52c5882dbe0450e9b3bd9766741a51f87
Larry Su <larry7696@gmail.com>
2018-11-07 16:18:25 +1300

    Update NettyServer.java

--- ikasoa-core/src/main/java/com/ikasoa/core/netty/server/NettyServer.java
+++ ikasoa-core/src/main/java/com/ikasoa/core/netty/server/NettyServer.java
@@ -9,12 +9,4 @@ import com.ikasoa.core.thrift.server.ThriftServer;
  * @version 0.6
  */
 public interface NettyServer extends ThriftServer {
-
-//	/**
-//	 * 获取Netty服务配置
-//	 * 
-//	 * @return NettyServerConfiguration 服务配置
-//	 */
-//	NettyServerConfiguration getNettyServerConfiguration();
-
 }
