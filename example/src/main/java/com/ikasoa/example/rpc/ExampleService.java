4147e468dec488e7a1d102d53de354e901e1853b
Larry <larry7696@gmail.com>
2020-03-29 09:47:11 +0800

    core 0.6.3

--- example/src/main/java/com/ikasoa/example/rpc/ExampleService.java
+++
@@ -1,6 +0,0 @@
-package com.ikasoa.example.rpc;
-
-public interface ExampleService {
-	// 查询对象
-	public ExampleVO findVO(int id);
-}
--- example/src/main/java/com/ikasoa/example/rpc/ExampleServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.ikasoa.example.rpc;
-
-public class ExampleServiceImpl implements ExampleService {
-	@Override
-	public ExampleVO findVO(int id) {
-		return new ExampleVO(id, "helloworld");
-	}
-}
